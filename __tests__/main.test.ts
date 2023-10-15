1035355c801c5baeee9d2b9f3122c23627a4e0f6
Max Baumann <max@bmn.dev>
2023-10-15 13:27:07 +0200

    implement pg-fingerprint-action

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -10,71 +10,49 @@ import * as core from '@actions/core'
 import * as main from '../src/main'
 
 // Mock the GitHub Actions core library
-const debugMock = jest.spyOn(core, 'debug')
 const getInputMock = jest.spyOn(core, 'getInput')
 const setFailedMock = jest.spyOn(core, 'setFailed')
-const setOutputMock = jest.spyOn(core, 'setOutput')
 
-// Mock the action's main function
-const runMock = jest.spyOn(main, 'run')
-
-// Other utilities
-const timeRegex = /^\d{2}:\d{2}:\d{2}/
+const runSpy = jest.spyOn(main, 'run')
 
 describe('action', () => {
+  beforeAll(() => {
+    process.chdir('./__tests__/sample-repo')
+  })
+
   beforeEach(() => {
+    process.env.GITHUB_BASE_REF = 'main'
     jest.clearAllMocks()
   })
 
-  it('sets the time output', async () => {
-    // Set the action's inputs as return values from core.getInput()
+  it('works', async () => {
     getInputMock.mockImplementation((name: string): string => {
       switch (name) {
-        case 'milliseconds':
-          return '500'
+        case 'files':
+          return '["file.sql"]'
         default:
           return ''
       }
     })
-
     await main.run()
-    expect(runMock).toHaveReturned()
 
-    // Verify that all of the core library functions were called correctly
-    expect(debugMock).toHaveBeenNthCalledWith(1, 'Waiting 500 milliseconds ...')
-    expect(debugMock).toHaveBeenNthCalledWith(
-      2,
-      expect.stringMatching(timeRegex)
-    )
-    expect(debugMock).toHaveBeenNthCalledWith(
-      3,
-      expect.stringMatching(timeRegex)
-    )
-    expect(setOutputMock).toHaveBeenNthCalledWith(
-      1,
-      'time',
-      expect.stringMatching(timeRegex)
-    )
+    expect(runSpy).toHaveReturned()
+    expect(setFailedMock).not.toHaveBeenCalled()
   })
 
-  it('sets a failed status', async () => {
-    // Set the action's inputs as return values from core.getInput()
+  it('errors', async () => {
     getInputMock.mockImplementation((name: string): string => {
       switch (name) {
-        case 'milliseconds':
-          return 'this is not a number'
+        case 'files':
+          return '["file.sql", "error.sql"]'
         default:
           return ''
       }
     })
 
     await main.run()
-    expect(runMock).toHaveReturned()
 
-    // Verify that all of the core library functions were called correctly
-    expect(setFailedMock).toHaveBeenNthCalledWith(
-      1,
-      'milliseconds not a number'
-    )
+    expect(runSpy).toHaveReturned()
+    expect(setFailedMock).toHaveBeenCalledWith('error.sql has changed!')
   })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { readFileSync } from 'fs'
 import * as core from '@actions/core'
 import { exec } from '@actions/exec'
-
-const parser = require('libpg-query');
+import * as parser from 'libpg-query'
 
 /**
  * The main function for the action.
@@ -11,51 +10,52 @@ const parser = require('libpg-query');
 export async function run(): Promise<void> {
   try {
     if (!process.env.GITHUB_BASE_REF) {
-      throw new Error("this action can only be triggered using a pull_request");
+      throw new Error('this action can only be triggered using a pull_request')
     }
 
-    const filesRaw: string = core.getInput('files', {required: true});
-    core.debug("filesRaw: " + filesRaw)
+    const filesRaw: string = core.getInput('files', { required: true })
+    core.debug(`filesRaw: ${filesRaw}`)
 
-    const filePaths: string[] = JSON.parse(filesRaw);
+    const filePaths: string[] = JSON.parse(filesRaw)
 
     for (const path of filePaths) {
-      const oldFile = await getOldFileContent(path);
-      const oldFP = await parser.fingerprint(oldFile);
-      core.debug(path + "[old]: " + oldFP);
+      const oldFile = await getOldFileContent(path)
+      core.debug(`${path}[old]: ${oldFile}`)
+      const oldFP = await parser.fingerprint(oldFile)
+      core.info(`${path}[old]: ${oldFP}`)
 
-      const newFile = readFileSync(path);
-      const newFP = await parser.fingerprint(newFile);
-      core.debug(path + "[new]: " + newFP);
-      
+      const newFile = readFileSync(path).toString()
+      core.debug(`${path}[new]: ${newFile}`)
+      const newFP = await parser.fingerprint(newFile)
+      core.info(`${path}[new]: ${newFP}`)
+
       if (newFP !== oldFP) {
-        throw new Error(path + " has changed!");
+        throw new Error(`${path} has changed!`)
       }
     }
-
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
 
 async function getOldFileContent(path: string): Promise<string> {
-  let oldFile = '';
-  let stdErr = '';
-  const command = `git show "${process.env.GITHUB_BASE_REF}:${path}"`;
+  let oldFile = ''
+  let stdErr = ''
+  const command = `git show "${process.env.GITHUB_BASE_REF}:${path}"`
   const options = {
     listeners: {
       stdout: (data: Buffer) => {
-        oldFile += data.toString();
+        oldFile += data.toString()
       },
       stderr: (data: Buffer) => {
-        stdErr += data.toString();
+        stdErr += data.toString()
       }
     }
   }
 
-  if (await exec(command, undefined, options) !== 0) {
-    throw new Error("could not exec " + command, {cause: stdErr});
+  if ((await exec(command, undefined, options)) !== 0) {
+    throw new Error(`could not exec '${command}'`, { cause: stdErr })
   }
 
-  return oldFile;
+  return oldFile
 }
